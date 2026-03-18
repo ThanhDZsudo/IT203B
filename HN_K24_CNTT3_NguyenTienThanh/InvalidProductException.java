1c523a1e2da6444e9172a4184d3b7859b2cea446
Thanh <thenngk6@gmail.com>
2026-03-18 13:11:55 +0700

    Delete HN_K24_CNTT3_NguyenTienThanh directory

--- HN_K24_CNTT3_NguyenTienThanh/InvalidProductException.java
+++
@@ -1,7 +0,0 @@
-package Ex5.HN_K24_CNTT3_NguyenTienThanh;
-
-public class InvalidProductException extends Exception {
-    public InvalidProductException(String message) {
-        super(message);
-    }
-}
